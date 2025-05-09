99670a6c7447ae7d32175463b2f8d0cf6f8e7bad
davJ-star <jshsy0130@gmail.com>
2025-05-09 15:36:46 +0900

    feat: update ForcastCtrl to handle forecast data retrieval via POST; enhance ForcastRequestDTO validation messages

--- src/main/java/com/example/demo/openapi/ctrl/ForcastCtrl.java
+++ src/main/java/com/example/demo/openapi/ctrl/ForcastCtrl.java
@@ -8,11 +8,18 @@ import com.example.demo.openapi.dto.ForcastResponseDTO;
 
 import jakarta.validation.Valid;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,24 +31,70 @@ public class ForcastCtrl {
     @Value("${openApi.serviceKey}")
     private String serviceKey;
 
-    @Value("${openApi.callBackUrls}")
-    private String callBackUrls;
+    @Value("${openApi.callBackUrl}")
+    private String callBackUrl;
 
     @Value("${openApi.dataType}")
     private String dataType;
 
 
-    @GetMapping("/getData")
-    public ResponseEntity<List<ForcastResponseDTO>> getData(@Valid @RequestBody ForcastRequestDTO params) {
+    @PostMapping("/getData")
+    public ResponseEntity<List<ForcastResponseDTO>> getData(@RequestBody ForcastRequestDTO params) {
         System.out.println("debug >>>> endpoint : /forcast/getData");
         System.out.println("debug >>>> serviceKey : " + serviceKey);
-        System.out.println("debug >>>> callBackUrls : " + callBackUrls);
+        System.out.println("debug >>>> callBackUrls : " + callBackUrl);
         System.out.println("debug >>>> dataType : " + dataType);
         System.out.println("debug >>>> params : " + params);
         // 얘가 callbackurl로 요청해야한다. 그리고 해당 값을 responseDTO로 담아서 전달해줘야한다.
+        // http://apis.data.go.kr/1360000/BeachInfoservice/getUltraSrtFcstBeach?beachNum=1&base_date=20220622&base_time=1230
+
+        String requestURL = callBackUrl
+                        +"?serviceKey="+serviceKey
+                        +"&beach_num="+ params.getBeach_num()
+                        +"&base_date="+ params.getBaes_date()
+                        +"&base_time="+ params.getBase_time();
+        System.out.println("debug >>>>>> requestURL: " + requestURL);                        
         // Service에서 처리하는게 좋지 않을까?
+        System.out.println("debug >>>> url : " + requestURL);
+        /////////
+        HttpURLConnection http   = null ; 
+        InputStream       stream = null ; 
+        String            result = null ;
+
+        List<ForcastResponseDTO>  list  = null ;
+        try {
+            URL url = new URL(requestURL);
+            http = (HttpURLConnection)url.openConnection();
+            System.out.println("http connection = " + http ); 
+            int code = http.getResponseCode() ; 
+            System.out.println("http response code  = " + code );  
+            if( code == 200 ) {
+                stream = http.getInputStream() ; // 응답데이터를 받는 코드다. (xml)
+                result = readString(stream) ;
+                System.out.println("result = " + result); 
+		
+                // 서비스 구현 
+            } else {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+        ///////// 
         return null;
     }
-    
+    public String readString(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        String input = null  ;
+        StringBuilder result = new StringBuilder();
+        while( (input = br.readLine() ) != null ) {
+            result.append(input+"\n\r");
+        }
+        br.close();   
+        return result.toString() ; 
+    }
     
 }
--- src/main/java/com/example/demo/openapi/dto/ForcastRequestDTO.java
+++ src/main/java/com/example/demo/openapi/dto/ForcastRequestDTO.java
@@ -6,13 +6,13 @@ import lombok.Data;
 @Data // ?? 왜 사용하는거지?
 public class ForcastRequestDTO {
     // @NotNull(message = "해변 번호가 없습니다.")
-    @NotNull(message = "해변 번호가 없습니다.")
+    @NotNull(message = "해변 번호가 없습니다. ) 1")
     private String beach_num;
     // @NotNull(message = "발표일자가 없습니다.")
-    @NotNull(message = "발표일자가 없습니다.")
+    @NotNull(message = "발표일자가 없습니다. ) 20220622")
     private String baes_date;
     // @NotNull(message = "발표시각이 없습니다.")
-    @NotNull(message = "발표시각이 없습니다.")
+    @NotNull(message = "발표시각이 없습니다. ) 1230")
     private String base_time;
 
     
